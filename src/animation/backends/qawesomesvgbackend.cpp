e9e6d6e22fd096cc74b14d91a1a9d70840bfdded
Ariane Djad <arianedjad@gmail.com>
2024-04-08 20:26:00 +0100

    feature(svg):add loadFromFile and loadFromData functions

--- src/animation/backends/qawesomesvgbackend.cpp
+++ src/animation/backends/qawesomesvgbackend.cpp
@@ -6,3 +6,21 @@ QAwesomeSvgBackend::QAwesomeSvgBackend(QObject* parent)
     : QAwesomeAnimationBackend(parent)
 {
 }
+
+bool QAwesomeSvgBackend::loadFromFile(const QString& path)
+{
+    m_renderers.clear();
+    m_single.reset(new QSvgRenderer(path));
+    if (m_single.isNull() || !m_single->isValid()) return false;
+    m_defaultSize = m_single->defaultSize();
+    return true;
+}
+
+bool QAwesomeSvgBackend::loadFromData(const QByteArray& data)
+{
+    m_renderers.clear();
+    m_single.reset(new QSvgRenderer(data));
+    if (m_single.isNull() || !m_single->isValid()) return false;
+    m_defaultSize = m_single->defaultSize();
+    return true;
+}
