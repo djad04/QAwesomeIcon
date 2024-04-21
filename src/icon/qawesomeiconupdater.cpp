421fdb4ad5cd87a6b75275a8a154b1bea9beec0b
Ariane Djad <arianedjad@gmail.com>
2024-04-22 00:47:00 +0100

    feature(iconUpdater):implement QAwesomeIconUpdater for dynamic widget and window icon management

--- src/icon/qawesomeiconupdater.cpp
+++ src/icon/qawesomeiconupdater.cpp
@@ -1,8 +1,58 @@
 #include "qawesomeiconupdater.h"
+#include <QWidget>
+#include <QWindow>
+#include <QGuiApplication>
+#include <QScreen>
 
 
-QAwesomeIconUpdater::QAwesomeIconUpdater(QObject *parent)
+QAwesomeIconUpdater::QAwesomeIconUpdater(QObject* parent)
+    : QObject(parent)
 {
-
 }
 
+void QAwesomeIconUpdater::attachToWidget(QWidget* widget, QAwesomeIconTargets targets)
+{
+    m_widget = widget;
+    m_window = nullptr;
+    m_targets = targets;
+}
+
+void QAwesomeIconUpdater::attachToWindow(QWindow* window, QAwesomeIconTargets targets)
+{
+    m_window = window;
+    m_widget = nullptr;
+    m_targets = targets;
+}
+
+void QAwesomeIconUpdater::detach()
+{
+    m_widget = nullptr;
+    m_window = nullptr;
+}
+
+void QAwesomeIconUpdater::setIconSizes(const QList<QSize>& sizes)
+{
+    m_sizes = sizes;
+}
+
+void QAwesomeIconUpdater::setDpr(qreal dpr)
+{
+    m_dpr = dpr;
+}
+
+QSize QAwesomeIconUpdater::targetSize() const
+{
+    if (!m_sizes.isEmpty()) return m_sizes.first();
+    return QSize(32, 32);
+}
+
+void QAwesomeIconUpdater::applyIcon(const QIcon& icon)
+{
+    if (m_widget) {
+        m_widget->setWindowIcon(icon);
+    }
+    if (m_window) {
+        m_window->setIcon(icon);
+    }
+    // TODO: Platform-specific taskbar icon updates or overlays
+}
--- src/icon/qawesomeiconupdater.h
+++ src/icon/qawesomeiconupdater.h
@@ -17,10 +17,26 @@ class QAwesomeIconUpdater : public QObject
 public:
     explicit QAwesomeIconUpdater(QObject* parent = nullptr);
 
+    void attachToWidget(QWidget* widget, QAwesomeIconTargets targets);
+    void attachToWindow(QWindow* window, QAwesomeIconTargets targets);
+    void detach();
+
+    void setIconSizes(const QList<QSize>& sizes);
+    void setDpr(qreal dpr);
+
+    QSize targetSize() const;
+
+public slots:
+    void onFrameReady(const QImage& image);
+
 private:
     QPointer<QWidget> m_widget;
     QPointer<QWindow> m_window;
+    QAwesomeIconTargets m_targets = QAwesomeIconTarget::All;
+    QList<QSize> m_sizes;
+    qreal m_dpr = 0.0;
 
+    void applyIcon(const QIcon& icon);
 };
 
 #endif // QAWESOMEICONUPDATER_H
